61d21c9fbb5ed859aab12cbb796805373aabf494
Gauri Mhaiskar <gauri.mhaiskar@gmail.com>
2021-08-10 13:42:14 -0400

    added routes and static files-general understanding of flask

--- server.py
+++ server.py
@@ -1,8 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-    return "<h1>Hello Gauri!</h1>"
+    # return "<h1>Hello Gauri!</h1>"
+    return render_template("./index.html")
+
+
+@app.route("/blog")
+def blog():
+    return "<h2>These are my thoughts..</h2>"
+
+
+@app.route("/blog/<username>")
+def blog_user(username="a default value"):
+    # return (
+    #     "<h1 style='color:red'>I'm not superstitious, but I am a little stitious</h1>"
+    # )
+    return render_template("./index.html", name=username)
+
+
+@app.route("/blog/<username>/<int:post_id>")
+def blog_fun(username="a default value", post_id=0):
+    # return (
+    #     "<h1 style='color:red'>I'm not superstitious, but I am a little stitious</h1>"
+    # )
+    return render_template("./index.html", name=username, id=post_id)
---
+++ static/script.js
@@ -0,0 +1,4 @@
+console.log('Hey there!');
+hEl = document.createElement('h1')
+hEl.textContent = "And Javascript too..🎃"
+document.body.appendChild(hEl)
